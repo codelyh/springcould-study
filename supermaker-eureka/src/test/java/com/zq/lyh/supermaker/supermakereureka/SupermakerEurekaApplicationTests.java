184b75fcb5362eefd6ea9ac74fd111e8c8ca0e53
liuyahui <329890023@qq.com>
2020-07-15 18:35:18 +0800

    first commit

---
+++ supermaker-eureka/src/test/java/com/zq/lyh/supermaker/supermakereureka/SupermakerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zq.lyh.supermaker.supermakereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermakerEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
