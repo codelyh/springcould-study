50fbc9de361e580b24441537469a57b4b43751b0
liuyahui <329890023@qq.com>
2020-07-16 18:05:16 +0800

    现在完成注册中心  和配置中心完整获取配置中心文件

---
+++ supermaker-config-client/src/test/java/com/zq/lyh/supermaker/supermakerconfigclient/SupermakerConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zq.lyh.supermaker.supermakerconfigclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermakerConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
