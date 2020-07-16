16d6afcf8bfeb59cc93c218d9bcf8a3d4335783c
liuyahui <329890023@qq.com>
2020-07-16 10:54:59 +0800

    eqweq

--- supermaker-eureka/src/test/java/com/zq/lyh/supermaker/supermakereureka/SupermakerEurekaApplicationTests.java
+++ supermaker-eureka/src/test/java/com/zq/lyh/supermaker/supermakereureka/SupermakerEurekaApplicationTests.java
@@ -1,6 +1,6 @@
 package com.zq.lyh.supermaker.supermakereureka;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
