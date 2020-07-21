227e863f47ede7eb31a8adbc4919c722f35cd7d9
liuyahui <329890023@qq.com>
2020-07-21 17:11:51 +0800

    增加zuul路由

---
+++ supermaker-zuul/src/test/java/com/zq/lyh/supermaker/SupermakerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zq.lyh.supermaker;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermakerZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
